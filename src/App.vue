a394ef94ad2ede086b058d20dd61fb1672e600d5
57code <yt0379@sohu.com>
2020-12-17 17:32:24 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <demo-zhihu></demo-zhihu>
+    <demo-tantan></demo-tantan>
+  </div>
+</template>
+
+<script>
+import DemoZhihu from './views/demo-zhihu.vue';
+import DemoTantan from './views/demo-tantan.vue';
+
+export default {
+  components: {
+    DemoZhihu,
+    DemoTantan
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
